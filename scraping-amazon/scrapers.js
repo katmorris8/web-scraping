41d3cab3c406c9256f22d5f832467bcdc90f17c3
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-07-22 17:41:52 -0400

    scraped the product price

--- scraping-amazon/scrapers.js
+++ scraping-amazon/scrapers.js
@@ -7,14 +7,18 @@ async function scrapeProduct(url) {
 
   const [el] = await page.$x('//*[@id="imgBlkFront"]');
   const src = await el.getProperty('src');
-  const srcTxt = await src.jsonValue();
+  const imageUrl = await src.jsonValue();
 
   const [el2] = await page.$x('//*[@id="productTitle"]');
   // const txt = await el2.getProperty('textContent');
   const txt = await el2.getProperty('innerHTML');
-  const rawTxt = await txt.jsonValue();
+  const title = await txt.jsonValue();
 
-  console.log({srcTxt, rawTxt});
+  const [el3] = await page.$x('//*[@id="price"]');
+  const txt2 = await el3.getProperty('innerHTML');
+  const price = await txt2.jsonValue();
+
+  console.log({imageUrl, title, price});
   
   browser.close()
 }
