0da3c4b0a5f1c20a8b38b9482868802eb01395f7
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-07-22 17:27:38 -0400

    scraped the image url path

--- scraping-amazon/scrapers.js
+++ scraping-amazon/scrapers.js
@@ -1,1 +1,17 @@
 const puppeteer = require('puppeteer');
+
+async function scrapeProduct(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const [el] = await page.$x('//*[@id="imgBlkFront"]');
+  const src = await el.getProperty('src');
+  const srcTxt = await src.jsonValue();
+
+  console.log({srcTxt});
+  
+  browser.close()
+}
+
+scrapeProduct('https://www.amazon.com/Brave-New-World-Aldous-Huxley/dp/0060850523/ref=sr_1_2')
